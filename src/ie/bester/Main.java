2685ae400d317e0aa4e394046c8072d291f254b2
paulbsr <paul.bester@outlook.com>
2020-12-24 01:07:19 +0000

    24 Dec

--- src/ie/bester/Main.java
+++ src/ie/bester/Main.java
@@ -2,13 +2,10 @@ package ie.bester;
 
 public class Main {
 
-    private static double mortgage;
-
     public static void main(String[] args) {
 
-
-        int hoofBedrag_P = 300000;
-        double jaarlikseRenteKoers_R = 3.0;
+        int hoofBedrag_P = 268000;
+        double jaarlikseRenteKoers_R = 3.6;
         byte termyn_N = 30;
         final byte maandeInJaar = 12;
         final byte persentasie = 100;
@@ -16,15 +13,8 @@ public class Main {
         float verbandTerugBetalings = termyn_N * maandeInJaar;
         double mortgage = hoofBedrag_P * (maandelikseRenteKoers_R * Math.pow(1 + maandelikseRenteKoers_R, verbandTerugBetalings))
                 / (Math.pow(1 + maandelikseRenteKoers_R, verbandTerugBetalings) - 1);
-        System.out.println(mortgage);
-    }
-        public static double getMortgage () {
-            System.out.println(mortgage);
-            return mortgage;
-        }
 
-
-    public static void setMortgage(double mortgage) {
-        Main.mortgage = mortgage;
+        System.out.println("Drolkop: " + mortgage);
     }
+
 }
